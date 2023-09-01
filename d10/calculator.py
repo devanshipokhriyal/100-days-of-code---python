66a7afb2c0a360f99e581e9c403ea8e6195b35af
devanshi <devanshipokhriyal2901@gmail.com>
2023-09-01 21:39:19 +0530

    cal using dict and func

--- d10/calculator.py
+++ d10/calculator.py
@@ -1,9 +1,31 @@
-def add(a,b):
-    sum=a+b
-    print (sum)
-def mul(a,b):
-    product=a*b
-    print(product)
-def div(a,b):
-    reminder=a/b
-    print(reminder)
+# add
+def add(n1,n2):
+  return n1+n2
+# subtract
+def sub(n1,n2):
+  return n1-n2
+# multiply
+def mul(n1, n2):
+  return n1*n2
+# Division
+def div(n1,n2):
+  return n1/n2
+
+# dictionary
+operations={
+  "+": add,
+  "-": sub,
+  "*": mul,
+  "/":div}
+
+num1=int(input("Enter the first number: \n"))
+num2=int(input("Enter the second number: \n"))
+
+for symbol in operations:
+  print(symbol)
+operation_symbol=input("Enter the operation to be performed:\n")
+
+calculation_function=  operations[operation_symbol]
+answer=(calculation_function(num1,num2))
+print(f"{num1} {operation_symbol} {num2} = {answer}")
+    
