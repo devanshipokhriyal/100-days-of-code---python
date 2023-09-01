b515820a036ec5062a879a0b12c6231ba40d9d41
devanshi <devanshipokhriyal2901@gmail.com>
2023-09-01 22:08:31 +0530

    looping it

--- d10/calculator.py
+++ d10/calculator.py
@@ -19,13 +19,24 @@ operations={
   "/":div}
 
 num1=int(input("Enter the first number: \n"))
-num2=int(input("Enter the second number: \n"))
+
 
 for symbol in operations:
   print(symbol)
-operation_symbol=input("Enter the operation to be performed:\n")
+should_continue=True
+while should_continue:
+ 
+  operation_symbol=input("Enter the operation to be performed:\n")
+  num2=int(input("Enter the next number: \n"))
+  
+  calculation_function=  operations[operation_symbol]
+  answer=(calculation_function(num1,num2))
+  print(f"{num1} {operation_symbol} {num2} = {answer}")
+  
+  if input("Do you want to continue with this answer? (y/n) \n")=="y":
+    num1=answer
+    # num2=int(input("Enter the next number: \n"))
+  else:
+    should_continue=False
+  
 
-calculation_function=  operations[operation_symbol]
-answer=(calculation_function(num1,num2))
-print(f"{num1} {operation_symbol} {num2} = {answer}")
-    
