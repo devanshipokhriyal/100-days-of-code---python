32eab0b3bf7914acef1e69f736467ad0c4997cb9
devanshi <devanshipokhriyal2901@gmail.com>
2023-10-08 20:50:36 +0530

    indexed + user friendly

--- d12/number_guess.py
+++ d12/number_guess.py
@@ -1,47 +1,42 @@
 import random
+from art import logo
 
-# def con(guess, number):
-#     if guess>number:
-#         print ("too high")
-#     if guess<number:
-#         print ("too - low")
-#     if guess==number:
-#         print ("correct")
-
-# def difficult():
-#     difficulty=input("select the difficulty- easy or hard?")
+# def dificulty():
+#     difficulty=input("select the difficulty- easy or hard?\n")
 #     if difficulty=="hard":
 #         attempts=5
 #     else:
 #         attempts=10
 #     return attempts
 
-
 number = random.randint(1, 100)
-print(number)
-# difficult()
-difficulty=input("select the difficulty- easy or hard?")
-if difficulty=="hard":
-     attempts=5
-else:
-     attempts=10
-    
-gameover=False
+print("Welcome to the Number Guessing Game\nIm thinking of a number between 1 and 100")
+# print(number)
+again=True
 
-while attempts!=0 and gameover!=True:
-    guess=int(input("guess the number: "))
-    # int(guess)
-    if guess>number:
-        print ("too high")
-    elif guess<number:
-        print ("too - low")
-    elif guess==number:
-        print ("correct")
-        gameover=True
-    # con("guess", "number")
-    attempts=attempts-1
+while again!=False:
 
+# dificulty() #attempts=10/5
 
-
-
+    difficulty=input("Select the difficulty- easy or hard?\n")
+    if difficulty=="hard":
+        attempts=5
+    else:
+        attempts=10
+    gameover=False
+    while attempts!=0 and gameover!=True:
+        guess=int(input("guess the number:\n ") )
+        if guess>number:
+            print ("too high")
+        elif guess<number:
+            print ("too low")
+        elif guess==number:
+            print ("correct")
+            gameover=True    
+        attempts=attempts-1
+    replay=input("do you want to replay? (y/n): \n")
+    if replay=="y":
+        again =True
+    else:
+        again=False
 
