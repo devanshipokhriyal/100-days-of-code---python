e2bd855b399fded612802b5b4282e2615dd8e5c1
devanshi <devanshipokhriyal2901@gmail.com>
2023-10-08 20:48:17 +0530

    ASCII art

--- d12/number_guess.py
+++ d12/number_guess.py
@@ -1,12 +1,12 @@
 import random
 
-def con():
-    if guess>number:
-        print ("too high")
-    elif guess<number:
-        print ("too low")
-    elif guess==number:
-        print ("correct")
+# def con(guess, number):
+#     if guess>number:
+#         print ("too high")
+#     if guess<number:
+#         print ("too - low")
+#     if guess==number:
+#         print ("correct")
 
 # def difficult():
 #     difficulty=input("select the difficulty- easy or hard?")
@@ -18,17 +18,28 @@ def con():
 
 
 number = random.randint(1, 100)
+print(number)
 # difficult()
 difficulty=input("select the difficulty- easy or hard?")
 if difficulty=="hard":
      attempts=5
 else:
      attempts=10
+    
+gameover=False
 
-while attempts!=0:
-    guess=input("guess the number: ")
-    con()
-    attempts-=attempts
+while attempts!=0 and gameover!=True:
+    guess=int(input("guess the number: "))
+    # int(guess)
+    if guess>number:
+        print ("too high")
+    elif guess<number:
+        print ("too - low")
+    elif guess==number:
+        print ("correct")
+        gameover=True
+    # con("guess", "number")
+    attempts=attempts-1
 
 
 
