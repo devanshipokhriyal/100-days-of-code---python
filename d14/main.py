56423547fbf88a9c9e410e9d7205d59398f41d05
devanshi <devanshipokhriyal2901@gmail.com>
2023-10-17 23:48:23 +0530

    -

--- d14/main.py
+++ d14/main.py
@@ -2,59 +2,64 @@ import random
 import art
 from game_data import data
 
-# def two():
-#     A = random.choice(data)
-#     B = random.choice(data)
-#     if A == B:
-#         B=random.choice(data)
-#     A_name=A["name"]
-#     A_desc=A["description"]
-#     A_country=A["country"]
-#     print(f"Compare A: name: {A_name } description: {A_desc} country: {A_country}")
-#     print(art.vs)
-#     B_name=B["name"]
-#     B_desc=B["description"]
-#     B_country=B["country"]
-#     print(f"Against B: name: {B_name } description: {B_desc} country: {B_country}")
+def format_data(account):
+    name=account["name"]
+    desc=account["description"]
+    country=account["country"]
+    return f"Name: {name }, Discription: {desc}, Country: {country}"
+
     
 
 
-# def check(Ans):
-#     if Ans == "A":
-#         A["follower_count"]>B["follower_count"]
+def check(Ans,A_followers, B_followers):
+    if A_followers>B_followers:
+        return ans=="a"
+    else:
+        return ans=="b"
+    # if Ans == "A":
+    #     A["follower_count"]>B["follower_count"]
    
 
-
-print(art.logo)
-A = random.choice(data)
-B = random.choice(data)
-if A == B:
-    B=random.choice(data)
-A_name=A["name"]
-A_desc=A["description"]
-A_country=A["country"]
-print(f"name: {A_name } description: {A_desc} country: {A_country}")
-print(art.vs)
-B_name=B["name"]
-B_desc=B["description"]
-B_country=B["country"]
-print(f"name: {B_name } description: {B_desc} country: {B_country}")
 score=0
-# two()
-ans=input("Who has more followers A or B?\n")
+print(art.logo)
+again=True
+B = random.choice(data)
+while again!=False:
+    A=B
 
-if ans == "A":
-    if A["follower_count"]>B["follower_count"]:
-        print("correct!")
+    B = random.choice(data)
+    # B = random.choice(data)
+    while A == B:
+        B=random.choice(data)
+    print(f"Compare A : {format_data(A)}")
+    print(art.vs)
+    print(f"Compare B : {format_data(B)}")
+
+    ans=input("Who has more followers A or B?\n").lower()
+    A_follower_count=A["follower_count"]
+    B_follower_count=B["follower_count"]
+
+    is_correct = check(ans, A_follower_count, B_follower_count)
+    if is_correct:
         score=score+1
-
+        print(f"correct!\nCurrent score: {score}")
+        
     else:
-        print(f"wrong!\n your score is {score}")
+        print(f"wrong!\nYour score is {score}")
+        again=False
+
+# if ans == "a":
+#     if A["follower_count"]>B["follower_count"]:
+#         print("correct!") 
+#         score=score+1
+
+#     else:
+#         print(f"wrong!\n your score is {score}")
       
-if ans == "B":
-    if B["follower_count"]>A["follower_count"]:
-        print("correct!")
-        score=score+1
-    else:
-        print(f"wrong!\n your score is {score}")
+# if ans == "b":
+#     if B["follower_count"]>A["follower_count"]:
+#         print("correct!")
+#         score=score+1
+#     else:
+#         print(f"wrong!\n your score is {score}")
         
