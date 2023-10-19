d05e8a3232fb24fc9f2c79521ca0e5520a56cdc4
devanshi <devanshipokhriyal2901@gmail.com>
2023-10-19 23:09:47 +0530

    coffee machine code

--- d16/coffee_machine.py
+++ d16/coffee_machine.py
@@ -28,10 +28,11 @@ resources = {
     "milk": 200,
     "coffee": 100,
     "water": 300,
+    "money":0,
     
 }
 # print(resources["milk"], resources["coffee"], resources["water"] )
-cost=0.0
+
 
 def total_cash(quat, dim, nick, penn): 
     quat = float(quat * 0.25)
@@ -41,34 +42,43 @@ def total_cash(quat, dim, nick, penn):
     t_cash = quat + dim + nick + penn
     return t_cash
 
+# def latte_recipe():
+#     if resources["milk"] >= 150 and resources["coffee"] >= 24 and resources["water"] >= 200:
+#         # global cost 
+#         resources["milk"] -= 200
+#         resources["coffee"] -= 24
+#         resources["water"] -= 150
+#         resources["money"] += 2.50
+#         return "Here's your order!\nHave a good day!"
+#     else:
+#         return "Sorry, not enough ingredients for a latte."
 def latte_recipe():
     if resources["milk"] >= 150 and resources["coffee"] >= 24 and resources["water"] >= 200:
-        global cost 
-        resources["milk"] -= 200
+        resources["milk"] -= 150
         resources["coffee"] -= 24
-        resources["water"] -= 150
-        cost += 2.50
+        resources["water"] -= 200
+        resources["money"] += 2.5
         return "Here's your order!\nHave a good day!"
     else:
         return "Sorry, not enough ingredients for a latte."
     
 def espresso_recipe():
-    global cost
+    # global cost
     if resources["coffee"] >= 18 and resources["water"] >= 50:
         resources["coffee"] -= 18
         resources["water"] -= 50
-        cost += 1.50
+        resources["money"]  += 1.50
         return "Here's your order!\nHave a good day!"
     else:
-        return "Sorry, not enough ingredients for a latte."
+        return "Sorry, not enough ingredients for an espresso."
 
 def cappuccino_recipe():
-    global cost
+    # global cost
     if resources["milk"] >= 100 and resources["coffee"] >= 24 and resources["water"] >= 250:
         resources["milk"] -= 100
         resources["coffee"] -= 24
         resources["water"] -= 250
-        cost += 3.0
+        resources["money"] += 3.0
         return "Here's your order!\nHave a good day!"
     else:
         return "Sorry, not enough ingredients for a latte."
@@ -79,8 +89,10 @@ while turn_off_machine!=True:
 
     order = input("What would you like? (espresso/latte/cappuccino):\n").lower()
     if order=="report":
-        print(f"Remaining:\nMILK: {resources['milk']}Liters\nCoffee: {resources['coffee']}grams\nWater: {resources['water']}Liters\n Cost: {cost}")
-    elif order!="report":
+        print(f"Remaining:\nMILK: {resources['milk']}Liters\nCoffee: {resources['coffee']}grams\nWater: {resources['water']}Liters\nMoney:{resources['money']} ")
+    elif order=="off":
+            turn_off_machine=True
+    elif order!="report" or order!="off":
         print("Insert coins:\n")
         quarter = int(input("How many quarters?"))
         dime = int(input("How many dimes?"))
@@ -95,23 +107,26 @@ while turn_off_machine!=True:
                 if total_money_inserted > 2.50:
                     remaining_balance= total_money_inserted -2.50
                     print(f"Here's the remaining balance: {remaining_balance:.2f}$")
+                    # print(f"Remaining:\nMILK: {resources['milk']}Liters\nCoffee: {resources['coffee']}grams\nWater: {resources['water']}Liters\nMoney:{resources['money']} ")
 
         elif order == "espresso":
             if total_money_inserted >= 1.5:
-                print(cappuccino_recipe())
+                print(espresso_recipe())
                 if total_money_inserted > 1.5:
                     remaining_balance= total_money_inserted -1.5
                     print(f"Here's the remaining balance: {remaining_balance:.2f}$")
+                    # print(f"Remaining:\nMILK: {resources['milk']}Liters\nCoffee: {resources['coffee']}grams\nWater: {resources['water']}Liters\nMoney:{resources['money']} ")
 
         elif order == "cappuccino":
             if total_money_inserted >= 3:
-                print(espresso_recipe())
+                print(cappuccino_recipe())
                 if total_money_inserted > 3:
                     remaining_balance= total_money_inserted -3
                     print(f"Here's the remaining balance: {remaining_balance:.2f}$")
+                    # print(f"Remaining:\nMILK: {resources['milk']}Liters\nCoffee: {resources['coffee']}grams\nWater: {resources['water']}Liters\nMoney:{resources['money']} ")
         
-        elif order=="off":
-            turn_off_machine=True
+        # elif order=="off":
+        #     turn_off_machine=True
 
 
 
